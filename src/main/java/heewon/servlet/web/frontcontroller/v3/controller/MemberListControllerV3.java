35eee8b1d249a4ee902c1bc2c9fe8d802afd818f
DESKTOP-FHGO7O0\gmldn <gmldnjs0402@naver.com>
2023-05-07 20:09:23 +0900

    Model 추가

---
+++ src/main/java/heewon/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,23 @@
+package heewon.servlet.web.frontcontroller.v3.controller;
+
+import heewon.servlet.domain.member.Member;
+import heewon.servlet.domain.member.MemberRepository;
+import heewon.servlet.web.frontcontroller.ModelView;
+import heewon.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+
+        return mv;
+    }
+}
